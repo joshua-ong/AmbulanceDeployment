799b6e8c5e415afe38ce07fe8bfc5d0d38ade528
guyFarmer <farmerguycf@utexas.edu>
2020-12-13 23:55:14 -0600

    ambulance_deployment_experiments updated to run all deployment models and fill .jld file. included team_stats.jld

--- AmbulanceDeployment.jl-legacy/src/deployment/malp.jl
+++ AmbulanceDeployment.jl-legacy/src/deployment/malp.jl
@@ -2,25 +2,29 @@ struct MALPDeployment <: DeploymentModel
     m::JuMP.Model
     x::Vector{JuMP.VariableRef}
 end
-deployment(m::MALPDeployment) = Int[round(Int,x) for x in JuMP.getValue(m.x)]
+deployment(m::MALPDeployment) = Int[round(Int,x) for x in JuMP.value.(m.x)]
 
 function MALPDeployment(p::DeploymentProblem,
                         q::Float64; # busy fraction;
                         α::Float64 = 0.99, # reliability level
                         tol = params.δ,
-                        solver = GurobiSolver(OutputFlag=0))
-    demand = vec(mean(p.demand[p.train,:],1))
+                        solver = Gurobi.Optimizer(OutputFlag=0))
+    demand = vec(mean(p.demand[p.train,:],dims = 1))
     @assert length(demand) == p.nregions
 
     I = 1:p.nlocations
     J = 1:p.nregions
     b = ceil(Int, log(1-α)/log(q))
 
-    m = JuMP.Model(solver=solver)
+    #m = JuMP.Model(solver=solver)
+    m = Model(GLPK.Optimizer)
+    solver=Gurobi.Optimizer(OutputFlag=0)
     JuMP.@variable(m, x[1:p.nlocations] >= 0, Int)
     JuMP.@variable(m, z[1:p.nregions, 1:b], Bin)
 
-    JuMP.@objective(m, Max, sum(demand[j]*z[j,b], j in J))
+
+    #possible breaking change with for j in J
+    JuMP.@objective(m, Max, sum(demand[j]*z[j,b] for j in J))
 
     JuMP.@constraint(m, sum(x[i] for i in I) <= p.nambulances)
     for j in J # coverage over all regions
@@ -34,5 +38,5 @@ function MALPDeployment(p::DeploymentProblem,
     MALPDeployment(m, x)
 end
 
-solve(model::MALPDeployment) = JuMP.solve(model.m)
-solve(model::MALPDeployment, p::DeploymentProblem) = JuMP.solve(model.m)
+optimize!(model::MALPDeployment) = JuMP.optimize!(model.m)
+optimize!(model::MALPDeployment, p::DeploymentProblem) = JuMP.optimize!(model.m)
--- AmbulanceDeployment.jl-legacy/src/deployment/stochastic.jl
+++ AmbulanceDeployment.jl-legacy/src/deployment/stochastic.jl
@@ -2,10 +2,11 @@ struct StochasticDeployment <: DeploymentModel
     m::JuMP.Model
     x::Vector{JuMP.VariableRef}
 end
-deployment(m::StochasticDeployment) = [round(Int,x) for x in JuMP.getvalue(m.x)]
+deployment(m::StochasticDeployment) = [round(Int,x) for x in JuMP.value.(m.x)]
 
 function StochasticDeployment(p::DeploymentProblem; nperiods=params.nperiods, tol=params.δ,
-    solver=GurobiSolver(OutputFlag=0))
+    #solver=GurobiSolver(OutputFlag=0))
+    solver=Gurobi.Optimizer(OutputFlag=0))
 
     nperiods = min(length(p.train), nperiods)
     demand = p.demand[p.train,:]
@@ -13,7 +14,7 @@ function StochasticDeployment(p::DeploymentProblem; nperiods=params.nperiods, to
     J = 1:p.nregions
     T = 1:nperiods
 
-    m = JuMP.Model(solver=solver)
+    m = Model(GLPK.Optimizer)
     JuMP.@variable(m, x[1:p.nlocations] >= 0, Int)
     JuMP.@variable(m, y[1:p.nlocations,1:p.nregions,1:nperiods] >= 0, Int)
     JuMP.@variable(m, z[1:p.nregions,1:nperiods] >= 0, Int)
@@ -28,18 +29,18 @@ function StochasticDeployment(p::DeploymentProblem; nperiods=params.nperiods, to
 
     # flow constraints at each station
     for i in I, t in T
-        JuMP.@expression(m, outflow, sum(y[i,j,t] for j in filter(j->p.coverage[j,i], J)))
+        outflow = JuMP.@expression(m, sum(y[i,j,t] for j in filter(j->p.coverage[j,i], J)))
         JuMP.@constraint(m, x[i] >= outflow)
     end
 
     # shortfall from satisfying demand/calls
     for j in J, t in T
-        JuMP.@expression(m, inflow, sum(y[i,j,t] for i in filter(i->p.coverage[j,i], I)))
+        inflow = JuMP.@expression(m, sum(y[i,j,t] for i in filter(i->p.coverage[j,i], I)))
         JuMP.@constraint(m, z[j,t] >= demand[t,j] - inflow)
     end
 
     StochasticDeployment(m, x)
 end
 
-solve(model::StochasticDeployment) = JuMP.solve(model.m)
-solve(model::StochasticDeployment, p::DeploymentProblem) = JuMP.solve(model.m)
+optimize!(model::StochasticDeployment) = JuMP.optimize!(model.m)
+optimize!(model::StochasticDeployment, p::DeploymentProblem) = JuMP.optimize!(model.m)
