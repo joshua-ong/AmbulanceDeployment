7ee5347480e40f58af69b82e2a63c740e874e301
guyFarmer <farmerguycf@utexas.edu>
2020-12-17 15:33:24 -0600

    resolved merge conflict

--- AmbulanceDeployment.jl-legacy/src/redeployment/assignment.jl
+++ AmbulanceDeployment.jl-legacy/src/redeployment/assignment.jl
@@ -1,11 +1,6 @@
 struct AssignmentModel <: RedeployModel
-<<<<<<< HEAD
+    model::Gurobi.Model
     #model::Gurobi.Model
-    model::Gurobi.Model
-=======
-    model::Gurobi.Model
-    #model::JuMP.Model
->>>>>>> 205c7e7a71edd3b070fcce685b1889b8f471c23a
     lambda::Float64
 
     hosp2stn::Matrix{Float64}
