2f7a1dc4dcc6861113bb325c31f5227725516c08
Sudeep Narala <sudeeprnarala@gmail.com>
2020-12-19 00:38:51 -0600

    Changed to work with austin data

--- AmbulanceDeployment.jl-legacy/src/Ambulance_Deployment_experiments.jl
+++ AmbulanceDeployment.jl-legacy/src/Ambulance_Deployment_experiments.jl
@@ -2,17 +2,25 @@ using AmbulanceDeployment
 using DataFrames, Winston, JLD, CSV, Gurobi, JuMP
 #include("problem.jl")
 # all of these paths are subject to where your data directory is located
-hourly_calls = CSV.File("../test/data/processed/2-weekday_calls.csv") |> DataFrame
+# hourly_calls = CSV.File("../test/data/processed/2-weekday_calls.csv") |> DataFrame
+# # weekend_hourly_calls = CSV.File("data/processed/2-weekend_calls.csv") |> DataFrame
+# adjacent_nbhd = CSV.File("../test/data/processed/2-adjacent_nbhd.csv") |> DataFrame
+# coverage = JLD.load("../test/data/processed/3-coverage.jld", "stn_coverage")
+# incidents = CSV.File("../test/data/processed/3-incidents_drivetime.csv") |> DataFrame
+
+hourly_calls = CSV.File("../test/austin-data/Full_WeekdayCalls.csv") |> DataFrame
 # weekend_hourly_calls = CSV.File("data/processed/2-weekend_calls.csv") |> DataFrame
-adjacent_nbhd = CSV.File("../test/data/processed/2-adjacent_nbhd.csv") |> DataFrame
-coverage = JLD.load("../test/data/processed/3-coverage.jld", "stn_coverage")
-incidents = CSV.File("../test/data/processed/3-incidents_drivetime.csv") |> DataFrame
+adjacent_nbhd = CSV.File("../test/austin-data/adjacent_nbhd.csv") |> DataFrame
+coverage = CSV.read("../test/austin-data/coverage_real.csv", DataFrame, header=false)
+coverage = convert(Array{Bool, 2}, coverage[:, :])
+incidents = CSV.File("../test/austin-data/austin_incidents.csv") |> DataFrame
 
 
-regions = Int[parse(Int,string(x)) for x in names(hourly_calls[:,5:ncol(hourly_calls)])]
+regions = Int[parse(Int,string(x)) for x in names(hourly_calls[:,6:ncol(hourly_calls)])]
 locations = collect(1:size(coverage,2))
 adjacent = convert(Array, adjacent_nbhd[:,2:ncol(adjacent_nbhd)])[regions,regions] .> 0.5
-demand = convert(Array,hourly_calls[:,5:end]);
+demand = convert(Array,hourly_calls[:,6:end]);
+
 
 incidents = incidents[.~ismissing.(incidents[!,:stn1_min]), :] # drop 44 calls that were "unreachable" (because all stations are reachable from each other)
 incidents[!,:interarrival_seconds] = [0; incidents[!,:arrival_seconds][2:end] - incidents[!,:arrival_seconds][1:end-1]]
@@ -43,7 +51,7 @@ DataFrames.first(calls, 6)
 # and the subsequent emergency calls from the remaining months as our test set
 peak_period = (hourly_calls[!,:hour] .>= 8) .* (hourly_calls[!,:hour] .<= 20)
 indices = 1:DataFrames.nrow(hourly_calls);
-train_filter = (hourly_calls[!,:year] .== 2012) .* (hourly_calls[!,:month] .<= 3)
+train_filter = (hourly_calls[!,:year] .== 2019) .* (hourly_calls[!,:month] .<= 3)
 test_filter  = .~train_filter;
 train_indices = indices[train_filter]
 test_indices = indices[test_filter];
@@ -52,7 +60,7 @@ test_indices = indices[test_filter];
 inc_peak_period = (calls[!,:hour] .>= 8) .* (calls[!,:hour] .<= 20)
 inc_indices = 1:DataFrames.nrow(calls);
 
-inc_train_filter = (calls[!,:year] .== 2012) .* (calls[!,:month] .<= 3)
+inc_train_filter = (calls[!,:year] .== 2019) .* (calls[!,:month] .<= 3)
 inc_test_filter  = .~inc_train_filter
 
 inc_train_indices = inc_indices[inc_train_filter]
@@ -83,39 +91,39 @@ test_inc_offpeak = .~inc_peak_period .* inc_test_filter;
     upptiming = Dict{Symbol, Dict{Int, Vector{Float64}}}()
     lowtiming = Dict{Symbol, Dict{Int, Vector{Float64}}}()
     amb_deployment = Dict{Symbol, Dict{Int, Vector{Int}}}()
-    for(deployment_model, name) in ((dp -> RobustDeployment(dp, α=0.1), :Robust01),
-                                      (dp -> RobustDeployment(dp, α=0.05), :Robust005),
-                                      (dp -> RobustDeployment(dp, α=0.01), :Robust001),
-                                      (dp -> RobustDeployment(dp, α=0.001), :Robust0001),
-                                      (dp -> RobustDeployment(dp, α=0.0001), :Robust00001))
-        println("$name: ")
-        amb_deployment[name] = Dict{Int, Vector{Int}}()
-        scenarios[name] = Dict{Int, Vector{Vector{Int}}}()
-        generated_deployment[name] = Dict{Int, Vector{Vector{Int}}}()
-        upperbounds[name] = Dict{Int, Vector{Float64}}()
-        lowerbounds[name] = Dict{Int, Vector{Float64}}()
-        upptiming[name] = Dict{Int, Vector{Float64}}()
-        lowtiming[name] = Dict{Int, Vector{Float64}}()
-        for namb in 10:5:50
-            println("$namb ")
-            p.nambulances = namb
-            model = deployment_model(p)
-            set_optimizer(model.m, Gurobi.Optimizer)
-            # solve(model, p)
-            @time optimize!(model, p)
-            #print("($(toq())) ")
-            amb_deployment[name][namb] = deployment(model)
-
-            # for tracking purposes
-            scenarios[name][namb] = model.scenarios
-            generated_deployment[name][namb] = model.deployment
-            upperbounds[name][namb] = model.upperbounds
-            lowerbounds[name][namb] = model.lowerbounds
-            upptiming[name][namb] = model.upptiming
-            lowtiming[name][namb] = model.lowtiming
-        end
-        println
-    end
+    # for(deployment_model, name) in ((dp -> RobustDeployment(dp, α=0.1), :Robust01),
+    #                                   (dp -> RobustDeployment(dp, α=0.05), :Robust005),
+    #                                   (dp -> RobustDeployment(dp, α=0.01), :Robust001),
+    #                                   (dp -> RobustDeployment(dp, α=0.001), :Robust0001),
+    #                                   (dp -> RobustDeployment(dp, α=0.0001), :Robust00001))
+    #     println("$name: ")
+    #     amb_deployment[name] = Dict{Int, Vector{Int}}()
+    #     scenarios[name] = Dict{Int, Vector{Vector{Int}}}()
+    #     generated_deployment[name] = Dict{Int, Vector{Vector{Int}}}()
+    #     upperbounds[name] = Dict{Int, Vector{Float64}}()
+    #     lowerbounds[name] = Dict{Int, Vector{Float64}}()
+    #     upptiming[name] = Dict{Int, Vector{Float64}}()
+    #     lowtiming[name] = Dict{Int, Vector{Float64}}()
+    #     for namb in 10:5:50
+    #         println("$namb ")
+    #         p.nambulances = namb
+    #         model = deployment_model(p)
+    #         set_optimizer(model.m, Gurobi.Optimizer)
+    #         # solve(model, p)
+    #         @time optimize!(model, p)
+    #         #print("($(toq())) ")
+    #         amb_deployment[name][namb] = deployment(model)
+    #
+    #         # for tracking purposes
+    #         scenarios[name][namb] = model.scenarios
+    #         generated_deployment[name][namb] = model.deployment
+    #         upperbounds[name][namb] = model.upperbounds
+    #         lowerbounds[name][namb] = model.lowerbounds
+    #         upptiming[name][namb] = model.upptiming
+    #         lowtiming[name][namb] = model.lowtiming
+    #     end
+    #     println
+    # end
 
 
              for (next_deployment_model, name) in ((next_dp -> StochasticDeployment(next_dp, nperiods=500), :Stochastic),
