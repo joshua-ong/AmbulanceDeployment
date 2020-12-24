82952141eca6aaf4206501d2e3825dfdb388976a
guyFarmer <farmerguycf@utexas.edu>
2020-12-24 11:08:47 -0600

    trying to run on austin data. coverage.csv needs conversion

--- AmbulanceDeployment.jl-legacy/test/response_time_store.jl
+++ AmbulanceDeployment.jl-legacy/test/response_time_store.jl
@@ -1,4 +1,4 @@
-using DataFrames, JLD, Distributions, CSV, Random, Plots
+using DataFrames, JLD, Distributions, CSV, Random, Plots,JSON
 import DataStructures: PriorityQueue, enqueue!, dequeue!
 include("..//src//model.jl")
 include("..//src//dispatch/closestdispatch.jl")
@@ -12,17 +12,27 @@ namb = 20
 lambda = 0
 
 local_path = ""
-hourly_calls = CSV.File(string(local_path,"../test/data/processed/2-weekday_calls.csv")) |> DataFrame
-adjacent_nbhd = CSV.File(string(local_path,"../test/data/processed/2-adjacent_nbhd.csv")) |> DataFrame
-coverage = JLD.load(string(local_path,"../test/data/processed/3-coverage.jld"), "stn_coverage")
-hospitals = CSV.File(string(local_path,"../test/data/processed/3-hospitals.csv")) |> DataFrame
-stations = CSV.File(string(local_path,"../test/data/processed/3-stations.csv")) |> DataFrame
- #solverstats = JLD.load(string(local_path,"data/processed/4-solve-stats.jld"))
-solverstats = JLD.load("team_stats.jld")
+# hourly_calls = CSV.File(string(local_path,"../test/data/processed/2-weekday_calls.csv")) |> DataFrame
+# adjacent_nbhd = CSV.File(string(local_path,"../test/data/processed/2-adjacent_nbhd.csv")) |> DataFrame
+# coverage = JLD.load(string(local_path,"../test/data/processed/3-coverage.jld"), "stn_coverage")
+# hospitals = CSV.File(string(local_path,"../test/data/processed/3-hospitals.csv")) |> DataFrame
+# stations = CSV.File(string(local_path,"../test/data/processed/3-stations.csv")) |> DataFrame
+
+hourly_calls = CSV.File("../test/austin-data/Full_WeekdayCalls.csv") |> DataFrame
+adjacent_nbhd = CSV.File("../test/austin-data/adjacent_nbhd.csv") |> DataFrame
+coverage = CSV.read("../test/austin-data/coverage_real.csv", DataFrame, header=false)
+incidents = CSV.File("../test/austin-data/austin_incidents.csv") |> DataFrame
+
+
+# solverstats = JLD.load(string(local_path,"data/processed/4-solve-stats.jld"))
+solverstats = JLD.load("../src/austin_team_stats.jld")
+# og_deployment = og_stats["amb_deployment"]
+# solverstats = JLD.load("team_stats.jld")
 amb_deployment = solverstats["amb_deployment"]
-const model_names = (:Stochastic, :Robust01, :Robust005, :Robust001, :Robust0001, :Robust00001, :MEXCLP, :MALP)
+const model_names = (:Stochastic, :MEXCLP, :MALP)
+# const model_names = (:Stochastic, :Robust01, :Robust005, :Robust001, :Robust0001, :Robust00001, :MEXCLP, :MALP)
 #const model_names = (:Stochastic, :Robust01,:MEXCLP, :MALP)
-model_namb = [25, 30, 35, 40, 45, 50] #note 10 breaks some assertion in simulation
+model_namb = [30, 35, 40, 45, 50] #note 10 breaks some assertion in simulation
 #name = model_names[1]
 
 p = DeploymentProblem(
@@ -30,7 +40,7 @@ p = DeploymentProblem(
     adjacent_nbhd,
     coverage,
     namb = namb,
-    train_filter = (hourly_calls[!,:year] .== 2012) .* (hourly_calls[!,:month] .<= 3)
+    train_filter = (hourly_calls[!,:year] .== 2020) .* (hourly_calls[!,:month] .<= 3)
 )
 
 # We focus on emergency calls during the "peak period" (8AM - 8PM),
@@ -47,7 +57,7 @@ result_dict = Dict{Symbol, Dict{Int, Vector{Float64}}}()
 #iterates through model (names) and number of ambulances for example Stochastic model with 20 ambulances
 # results = Array{Float64,2}(undef, 8, 5) #it saves the results to print later
 results = Any[]
-for j = 1:8
+for j = 1:3
     # model_results = Any[]
     result_dict[model_names[j]] = Dict{Int, Vector{Int}}()
     for i = 1:5
@@ -70,20 +80,11 @@ for j = 1:8
     end
 end
 
-# plot(model_namb, adjoint(results[:,:]), markershape = :ltriangle, label = "stochastic robust01 ")
-# xlabel!("Number of Ambulances")
-# ylabel!("Mean Response Time")
-# savefig("og_collectiveplot.png")
-JLD.jldopen("response_times.jld", "w") do file
+
+JLD.jldopen("../src/outputs/austin_response_times.jld", "w") do file
     write(file, "response_times", result_dict)
 end
-# x = amb_deployment[name][namb]
-# problem = DispatchProblem(test_calls, hospitals, stations, p.coverage, x, turnaround=turnaround)
-# dispatch = ClosestDispatch(p, problem)
-# redeploy = AssignmentModel(p, x, hospitals, stations, lambda=Float64(lambda))
-#
-# # id 145 dispatch to nbhd 88
-# Random.seed!(1234); # reset seed
-# @time df = simulate_events!(problem, dispatch, redeploy);
-# @show mean(df[!,:waittime]), maximum(df[!,:waittime])
-# @show mean(df[!,:waittime] + df[!,:responsetime])
+json_string = JSON.json(result_dict)
+open("../src/outputs/austin_response_times.json","w") do f
+    write(f, json_string)
+end
